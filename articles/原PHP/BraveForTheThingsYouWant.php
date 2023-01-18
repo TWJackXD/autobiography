3d4c3f56e88e4968d8dd2443b31044bb421d3b09
TWJackXD <105576692+TWJackXD@users.noreply.github.com>
2023-01-18 21:57:38 +0800

    Add files via upload

---
+++ articles/原PHP/BraveForTheThingsYouWant.php
@@ -0,0 +1,131 @@
+<?php
+$style = "/*Custom CSS*/";
+$title = "我不勇敢，但我願意為我要的，勇敢這一次。";
+$date = "2022-01-21";
+$cat = "長篇文章";
+$content = '
+<h2>正文</h2>
+<p>《我不勇敢，但我願意為我要的，勇敢這一次》</p>
+
+<p>二零二一年十二月四日，那天是星期六，我當時在客廳滑著手機，偶然間聽到爸爸跟姐姐的談話內容。一開始他們在講男女朋友的事，我就沒有多去理會，但再來我很確定，姐姐一定是為了我，而問了爸爸：「你支持同性戀嗎？」聽到這句話，我趕緊躲到房門後聽，直到聽到答案，才敢進去聽完……。</p>
+
+<p>「在傳統觀念中，男女生在一起才符合道德倫理，因為男女結合才能產生後代，女女至少還能向別人借精，但男生就只能領養（#註1），而且在這觀念中，同性戀是一種病。」</p>
+
+<p>其實這些我全部都知道，為了搞清楚自己的性向—雙性戀，我早就查了幾十、幾百篇資料，中、英文都看了個遍。看完後，我也十分堅定自己會喜歡男生，也確定自己就是雙性戀。</p>
+
+<p>但說到這裡，心中的問號也解開了，也下定決心要告訴爸爸關於我自己是雙性戀的事。經過一番思考後，我決定寫在聯絡簿上的札記，順便回答老師之前的問題，一次向爸爸與老師出櫃（#註2）。要，就乾脆一點吧！</p>
+
+<p>先前有跟某個朋友討論一次，她是個同性戀，而她家人不支持同性戀，所以她說很擔心我。但在我跟她聊過之後，她也看到了我的決心。我傳了一則訊息，一則代表我很認真要出櫃的訊息—「我不勇敢，但我願意為我要的，勇敢這一次。」</p>
+
+<p>十二月五日，晚上十點十分，我開始寫起了札記，這是我第一次寫這麼長的札記，裡面包括少部分研究的內容、自己的親身經歷等。寫了快一個小時，直到十點五十分才完成。</p>
+
+<p>之後又思考了幾分鐘，準備好後，我敲了爸爸房間的門。一開始爸爸還很生氣，說：「下次聯絡簿早點拿出來」，而下一秒，他看了貼在上面的札記，就收了脾氣，看了下去，還轉成笑臉，問了我：「你是雙性戀喔？」叫我把門關起來，說要談一下。</p>
+
+<p>後來就跟爸爸談了一下……</p>
+
+<p>「我沒有支持或不支持，反正同性婚姻都已經合法了。如果你喜歡同性，我也不會說什麼，反正不管男女，未來都是你們的。但你要知道，同性戀更容易得病，你也要知道自己是0還是1（#註3），要知道怎麼保護自己。而且未來你也可能交到一個不錯的女孩子也說不定，你們這個階段會對性向還在摸索也很正常，所以不用太早下定論。但就算你以後真的交了男朋友，那也不關我們的事，有錢給我們養老就好了。」</p>
+
+<p>0跟1，其實自己知道就好。而摸索階段？可能吧！但我說真的，誰會無聊到硬要說自己不是異性戀？誰會無聊到刻意去喜歡一個同性？誰會無聊到想讓自己的權利不如異性戀高？我的異性戀朋友，對同性就是一點感覺都沒有，而我，就是真心會對同性有感覺，甚至身體或心理自然也會有動作（例如：心跳加速等）。研究這些下來，我也得出：不會變就是不會變，我很確定，我就是這樣，一直都是，只是以往都被長輩的舊價值觀限制住罷了。</p>
+
+<p>而說實話，他的每一字、每一句，我都猜到了。因為之前看了許多文章、資料，我也是想了所有可能性，才決定出櫃的，所以他說的話，我早就看過了。但他說的一個成語卻讓我忘也忘不掉──「順其自然」，雖然跟我想的一樣，但從家長口中說出來，卻使我格外放心。</p>
+
+<p>談完後，回到房間，我開心的噴了一滴淚，也立刻向朋友報告「喜訊」，她說：「你成功了，我的朋友！」還說感動到快哭了、前陣子很擔心我。也非常謝謝這位朋友給我的打氣！</p>
+
+<p>我之後也打算找時間跟健康老師報告，並好好的感謝她。幫我這麼多，也該好好道謝了！其實原本是說出櫃前再和健康老師談一下，她也說「不希望我受傷」，但這次基於大大小小的原因，一個衝動就出櫃了，也慶幸是個完美結局……。</p>
+
+<p>「我不勇敢，但我願意為了我要的，勇敢這一次」，而我，經歷了快一整年的心路歷程，終於，成功了。</p>
+
+<p>—小狼呆呆 2021.12.08 @LGBTQ+<br/>
+Brave for the things you want.</p>
+
+<h2>#註解</h2>
+<p>1 – 目前男同伴侶只可繼親收養，不能直接領養。</p>
+<p>2 – 表示向他人公布自己的性向（非異性戀）。</p>
+<p>3 – 0表示男同志之受，1表示男同志之攻。</p>
+';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+	<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap" rel="stylesheet">
+	<link href="../css/style.css" rel="stylesheet">
+	<title><?php echo($title)?> - 我是呆呆我好爛</title>
+	<link href="../img/icon.png" rel="icon">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta name="description" content="來看看弱爆的呆呆在幹嘛吧！" />
+</head>
+<body>
+	<!-- Navbar -->
+	<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+		<div class="container">
+		    <a class="navbar-brand" href="../index.html">我是呆呆我好爛</a>
+		    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#NavToggler">
+		    	<span class="navbar-toggler-icon"></span>
+		    </button>
+			<div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="NavToggler">
+				<div class="offcanvas-header">
+					<h5 class="offcanvas-title" id="NavToggler">我是呆呆我好爛</h5>
+					<button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"></button>
+				</div>
+				<div class="offcanvas-body">
+					<ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+						<li class="nav-item">
+							<a class="nav-link mR20" href="../index.html"><i class="fa-solid fa-globe"></i> 首頁</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link mR20" style="cursor: not-allowed;"><i class="fa-solid fa-user-large"></i> 關於我</a>
+						</li>
+
+
+						<li class="nav-item dropdown mR20">
+							<a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+							<i class="fa-solid fa-newspaper"></i> 文章堆
+							</a>
+							<ul class="dropdown-menu">
+								<li><a class="dropdown-item" href="thePlanet.html">歌曲分享</a></li>
+								<li><a class="dropdown-item" href="#">長篇文章</a></li>
+							</ul>
+						</li>
+
+
+						<li class="nav-item">
+							<a class="nav-link" style="cursor: not-allowed;"><i class="fas fa-envelope"></i> 聯絡我</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</nav>
+
+	<!-- Banner -->
+	<div class="container-fluid text-light" style="background: #333333;">
+		<div class="row align-items-center text-center banner">
+			<div class="col">
+				<h2><?php echo($title)?></h2>
+				<span>小狼呆呆．<?php echo($date)?>．<?php echo($cat)?></span>
+			</div>
+		</div>
+	</div>
+
+	<!-- Body -->
+	<div class="container mTop30">
+		<center>
+			<div class="col-sm-9" style="text-align: left;">
+				<?php echo($content)?>
+			</div>
+		</center>
+	</div>
+
+	<!-- Footer -->
+	<footer class="footer text-center text-bg-dark mTop30" style="padding: 10px">
+		copyright © Jack_XD <script>document.write(new Date().getFullYear())</script><br>
+		版面參考 <a href="https://yuyutw.tk" target="_blank">[佑仔 - 個人網誌Demo]</a>
+	</footer>
+</body>
+<style><?php echo($style)?></style>
